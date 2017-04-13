27a4775093d87a4648bdb02bf1bbe56a2d2261d6
Rawin Deeboonchai <comtalyst@outlook.com>
2017-04-13 15:03:25 +0700

    Add Capture Function
    Add Capture & Display functions to the application which capture current
    screen and display it in application's UI via JavaFX

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Controller {
+    @FXML
+    private ImageView capImage;
+
+    @FXML
+    public void capture(){
+        double screenH = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        double screenW = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        Rectangle border = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());      // Set Capture border
+        BufferedImage screen = null;
+        try {
+            screen = new Robot().createScreenCapture(border);                               // Capture!
+        }catch (AWTException e){
+            e.printStackTrace();
+        }
+        capImage.setFitHeight(screenH/1.5);
+        capImage.setFitWidth(screenW/1.5);
+        capImage.setImage(SwingFXUtils.toFXImage(screen, null));
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,15 +7,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static Stage nstage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 950, 600));
         primaryStage.show();
+        nstage = primaryStage;
     }
-
+    public static Stage getStage(){
+        return nstage;
+    }
 
     public static void main(String[] args) {
         launch(args);
