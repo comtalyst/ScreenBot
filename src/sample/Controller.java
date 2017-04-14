89750e70105535fc4b2832288ea5cbb3a93fc7d4
Rawin Deeboonchai <comtalyst@outlook.com>
2017-04-14 12:53:40 +0700

    Service system testing
    Testing service function to do a background task

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,35 +1,79 @@
 package sample;
 
-import javafx.embed.swing.SwingFXUtils;
+import javafx.application.Platform;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.beans.EventHandler;
+import java.util.concurrent.CountDownLatch;
+import javafx.scene.control.Label;
 
 public class Controller {
     @FXML
-    private ImageView capImage;
+    Label label;
 
-    @FXML
-    public void capture(){
-        double screenH = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-        double screenW = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-        Rectangle border = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());      // Set Capture border
-        BufferedImage screen = null;
-        try {
-            screen = new Robot().createScreenCapture(border);                               // Capture!
-        }catch (AWTException e){
-            e.printStackTrace();
+    boolean state = true;
+
+    Service<Void> looper = new Service<Void>() {                                                        // Create service to loop
+        protected Task<Void> createTask() {                                                             // in the background
+            return new Task<Void>() {
+                public Void call() throws Exception{
+                    double screenH = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+                    double screenW = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+                    Rectangle border = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());      // Set Capture border
+                    BufferedImage screen = null;                                                        // Prepare image vessel
+                    int a = 0;
+                    final CountDownLatch latch = new CountDownLatch(1);                                 // Prepare prototype latch
+                    while (state) {
+                        a++;
+                        final int b = a;
+                        final CountDownLatch latch2 = latch;
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                System.out.println("Shit");
+                                try{
+                                    editFx(Integer.toString(b));
+                                }finally{
+                                    latch2.countDown();
+                                }
+
+                            }
+                        });
+                        latch2.await();
+                        try {
+                            screen = new Robot().createScreenCapture(border);                           // Continuous Capturing
+                        } catch (AWTException e) {
+                            e.printStackTrace();
+                        }
+                        // Find average color of screen (this function will be added later)
+                    }
+                    return null;
+                }
+            };
         }
-        capImage.setFitHeight(screenH/1.5);
-        capImage.setFitWidth(screenW/1.5);
-        capImage.setImage(SwingFXUtils.toFXImage(screen, null));
+    };
+
+    public void start(){
+        if(looper.getState().equals(Worker.State.RUNNING)){
+            looper.cancel();
+        }
+        looper.restart();
+    }
+    private void editFx(String s){
+        System.out.println('>' + s);
+        label.setText(s);
+    }
+
+    public void stop(){
+        if(looper.getState().equals(Worker.State.RUNNING)){
+            looper.cancel();
+        }
+        System.out.println("Stop Initiated");
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 950, 600));
+        primaryStage.setScene(new Scene(root, 140, 80));
         primaryStage.show();
         nstage = primaryStage;
     }
